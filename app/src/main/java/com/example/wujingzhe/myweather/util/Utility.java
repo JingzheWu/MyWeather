3d9899ddf2b9bd5aeb73491e45b30d196babd17d
wujingzhe <1370819501@qq.com>
2016-10-04 13:23:40 +0800

    增加解析、存储JSON数据方法

--- app/src/main/java/com/example/wujingzhe/myweather/util/Utility.java
+++ app/src/main/java/com/example/wujingzhe/myweather/util/Utility.java
@@ -1,11 +1,24 @@
 package com.example.wujingzhe.myweather.util;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.SearchRecentSuggestionsProvider;
+import android.content.SharedPreferences;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.example.wujingzhe.myweather.model.City;
 import com.example.wujingzhe.myweather.model.County;
 import com.example.wujingzhe.myweather.model.MyWeatherDB;
 import com.example.wujingzhe.myweather.model.Province;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by wujingzhe on 2016/10/3.
@@ -72,6 +85,40 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    //解析服务器返回的JSON数据
+    public static void handleWeatherResponse(Context context, String response){
+        try{
+            JSONObject jsonObject=new JSONObject(response);
+            JSONObject weatherInfo=jsonObject.getJSONObject("weatherinfo");
+            String cityName=weatherInfo.getString("city");
+            String weatherCode=weatherInfo.getString("cityid");
+            String temp1=weatherInfo.getString("temp1");
+            String temp2=weatherInfo.getString("temp2");
+            String weatherDesp=weatherInfo.getString("weather");
+            String publishTime=weatherInfo.getString("ptime");
+            saveWeatherInfo(context,cityName,weatherCode,temp1,temp2,weatherDesp,publishTime);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    //将解析后的JSON数据存储到SharePreferences文件中
+    @TargetApi(Build.VERSION_CODES.N)
+    public static void saveWeatherInfo(Context context, String cityName, String weatherCode, String temp1,
+                                       String temp2, String weatherDesp, String publishTime){
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy年M月d日",Locale.CHINA);
+        SharedPreferences.Editor editor= PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_selected",true);
+        editor.putString("city_name",cityName);
+        editor.putString("weather_code",weatherCode);
+        editor.putString("temp1",temp1);
+        editor.putString("temp2",temp2);
+        editor.putString("weather_desp",weatherDesp);
+        editor.putString("publish_time",publishTime);
+        editor.putString("current_date",sdf.format(new Date()));
+        editor.commit();
     }
 
 }
